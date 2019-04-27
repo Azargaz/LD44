f51902a8285bd562687eafdc7039f7359132a752
azargaz <huba310@gmail.com>
2019-04-27 13:01:21 +0200

    Basic Level generation ready.

--- Assets/Scripts/LevelGeneration.cs
+++ Assets/Scripts/LevelGeneration.cs
@@ -4,6 +4,43 @@ using UnityEngine;
 
 public class LevelGeneration : MonoBehaviour
 {
-    
+    public int numberOfRooms;
+
+    public GameObject[] rooms;
+    public int roomsSize = 8;
+
+    public int wallsSize = 4;
+    public GameObject walls;
+
+    void Start()
+    {
+        Generate();
+    }
+
+    void Generate()
+    {
+        Vector2 spawnPos = new Vector2(0, 0);
+        int roomsLeft = numberOfRooms;
+        while(roomsLeft > 0)
+        {
+            int randomIndex = Random.Range(0, rooms.Length);
+            Instantiate(rooms[randomIndex], spawnPos, Quaternion.identity, transform);
+        
+            spawnPos += new Vector2(8, 0);
+            roomsLeft--;
+        }
+
+        for(int x = -wallsSize; x < numberOfRooms * roomsSize + wallsSize; x++)
+        {
+            for(int y = -wallsSize; y < wallsSize + roomsSize; y++)
+            {
+                if(x >= 0 && x < numberOfRooms * roomsSize && y >= 0 && y < roomsSize)
+                    continue;
+
+                spawnPos = new Vector2(x, y);
+                Instantiate(walls, spawnPos, Quaternion.identity, transform);
+            }
+        }
+    }
 
 }
